4135710344d3bd7e5b3557690cc5c37b6618f292
niqdev <niqdev@users.noreply.github.com>
2021-03-26 13:21:50 +0000

    draft intro module

--- modules/intro/build.gradle.kts
+++ modules/intro/build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
+  application
   id("org.jetbrains.kotlin.jvm") version Versions.kotlin
   id("org.jmailen.kotlinter") version Versions.kotlinter
 }
 
 repositories {
   jcenter()
+}
+
+// TODO ./gradlew :modules:app:run
+// TODO ./gradlew run -Pmain=com.github.niqdev.HelloWorld
+application {
+  mainClass.set(project.findProperty("main").toString())
 }
