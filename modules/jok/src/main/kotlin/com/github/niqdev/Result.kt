9cbe90edcc3f79958eecc7045234196b26462d73
niqdev <niqdev@users.noreply.github.com>
2021-05-18 18:08:43 +0100

    update JoyOfKotlinChapter07

--- modules/jok/src/main/kotlin/com/github/niqdev/Result.kt
+++ modules/jok/src/main/kotlin/com/github/niqdev/Result.kt
@@ -87,9 +87,81 @@ fun <A> Result<A>.filter(): ((A) -> Boolean) -> Result<A> =
 fun <A> Result<A>.exists(): ((A) -> Boolean) -> Boolean =
   { p -> this.map<A, Boolean>()(p).getOrElse()() { false } }
 
+// ---------- 7.7 ----------
+
+fun <A> Result<A>.mapFailure(): (String) -> Result<A> =
+  { error ->
+    when (this) {
+      is Result.Empty -> Result.Empty
+      is Result.Success -> Result.Success(this.value)
+      is Result.Failure -> Result.Failure(RuntimeException(error, this.exception))
+    }
+  }
+
+// ---------- 7.9 ----------
+
+fun <A> Result<A>.unsafeForEach(): ((A) -> Unit) -> Unit =
+  { effect ->
+    when (this) {
+      is Result.Empty -> Result.Empty
+      is Result.Failure -> Result.Failure<A>(this.exception)
+      is Result.Success -> effect(this.value)
+    }
+  }
+
+// ---------- 7.10 ----------
+// ---------- 7.11 ----------
+
+fun <A> Result<A>.unsafeForEachOrElse(): ((A) -> Unit) -> ((RuntimeException) -> Unit) -> (() -> Unit) -> Unit =
+  { onSuccess ->
+    { onFailure ->
+      { onEmpty ->
+        when (this) {
+          is Result.Empty -> onEmpty()
+          is Result.Failure -> onFailure(this.exception)
+          is Result.Success -> onSuccess(this.value)
+        }
+      }
+    }
+  }
+
+// ---------- 7.12 ----------
+
+fun <A, B> ((A) -> B).liftResult(): (Result<A>) -> Result<B> =
+  { result -> result.map<A, B>()() { a -> this(a) } }
+
+// ---------- 7.13 ----------
+
+fun <A, B, C> ((A) -> (B) -> C).lift2Result(): (Result<A>) -> (Result<B>) -> Result<C> =
+  { resultA -> { resultB -> resultA.flatMap<A, C>()() { a -> resultB.map<B, C>()() { b -> this(a)(b) } } } }
+
+fun <A, B, C, D> ((A) -> (B) -> (C) -> D).lift3Result(): (Result<A>) -> (Result<B>) -> (Result<C>) -> Result<D> =
+  { resultA ->
+    { resultB ->
+      { resultC ->
+        resultA.flatMap<A, D>()() { a ->
+          resultB.flatMap<B, D>()() { b ->
+            resultC.map<C, D>()() { c ->
+              this(a)(b)(c)
+            }
+          }
+        }
+      }
+    }
+  }
+
+// ---------- 7.14 ----------
+
+fun <A, B, C> Result<A>.map2(): (Result<B>) -> ((A) -> (B) -> C) -> Result<C> =
+  { resultB -> { f -> f.lift2Result()(this)(resultB) } }
+
+// specify the types because of the limited type inference capacity of Kotlin
+// use comprehension pattern to compose: N flatMap + map
 fun main() {
   println(Result(42).filter()() { it < 100 })
   println(Result(42).filter()() { it > 100 })
   println(Result(42).exists()() { it < 100 })
   println(Result<Int>().exists()() { it < 100 })
+  println(Result(42).unsafeForEach()(::println))
+  println(Result.failure<Int>("error").unsafeForEachOrElse()(::println)(::println)() { println("empty") })
 }
