8d20a3492fd23bebf43a983c869a60583eed3180
niqdev <niqdev@users.noreply.github.com>
2021-06-22 12:29:48 +0100

    update versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,6 @@
 object Versions {
   // plugins
   val kotlin = "1.5.10"
-  val kotlinxCoroutines = "1.5.0"
   val kotlinxSerialization = "1.2.1"
   val kotlinter = "3.4.4"
   val kapt = "1.5.10"
@@ -11,13 +10,15 @@ object Versions {
   val aws = "2.16.80"
   val awsLambdaCore = "1.2.1"
   val awsLambdaEvents = "3.9.0"
-
   val arrow = "0.13.2"
   val clikt = "3.2.0"
-  val hoplite = "1.4.0"
+  val hoplite = "1.4.3"
+  val kotlinxCoroutines = "1.5.0"
   val logback = "1.2.3"
   val reactor = "3.4.6"
   val reactorKotlin = "1.1.3"
   val slf4j = "1.7.30"
+
+  // tests
   val kotest = "4.6.0"
 }
