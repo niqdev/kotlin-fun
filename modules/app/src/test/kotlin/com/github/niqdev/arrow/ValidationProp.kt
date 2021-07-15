4825fa20f892e731c396e4db0134635168497e76
niqdev <niqdev@users.noreply.github.com>
2021-07-15 12:52:45 +0100

    update ValidationProp

--- modules/app/src/test/kotlin/com/github/niqdev/arrow/ValidationProp.kt
+++ modules/app/src/test/kotlin/com/github/niqdev/arrow/ValidationProp.kt
@@ -1,12 +1,43 @@
 package com.github.niqdev.arrow
 
+import arrow.core.left
+import arrow.core.right
+import com.github.niqdev.arrow.EitherExample.validateName
+import com.github.niqdev.arrow.ExampleGenerators.exampleArb
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.string.shouldHaveLength
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.*
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
 
+object ExampleGenerators {
+
+  val emailArb = Arb.string().flatMap { prefix ->
+    Arb.string().map { suffix ->
+      "$prefix@$suffix"
+    }
+  }
+
+  val exampleArb = Arb.bind(
+    Arb.string(),
+    Arb.int(),
+    emailArb
+  ) { name, age, email -> ExampleForm(name, age, email) }
+}
+
 class ValidationProp : StringSpec({
 
+  "validate name" {
+    forAll(exampleArb) { form ->
+      when {
+        form.name.isBlank() -> form.validateName() == FormError.BlankName.left()
+        else -> form.validateName() == form.name.right()
+      }
+    }
+  }
+
+  // example only
   "String size" {
     forAll<String, String> { a, b ->
       (a + b).length == a.length + b.length
