c8ec02ad80b7a34261b5e3edef01ce4b26352008
niqdev <niqdev@users.noreply.github.com>
2021-07-26 15:39:46 +0100

    update minio

--- modules/aws-local/src/main/kotlin/com/github/niqdev/aws.kt
+++ modules/aws-local/src/main/kotlin/com/github/niqdev/aws.kt
@@ -5,6 +5,19 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 
+/*
+
+# setup minio profile
+aws configure --profile minio
+#AWS Access Key ID [None]: LOCAL_ACCESS_KEY
+#AWS Secret Access Key [None]: LOCAL_SECRET_KEY
+#Default region name [None]: us-east-1
+#Default output format [None]:
+
+aws --profile minio --endpoint-url http://localhost:9000 s3 mb s3://my-bucket
+aws --profile minio --endpoint-url http://localhost:9000 s3 cp --recursive ./local/data/<PATH> s3://my-bucket
+
+*/
 object aws {
 
   val localstackUrl = "http://localhost:4566"
