190f76caddc18513859ebc3988a9622c1368a250
niqdev <niqdev@users.noreply.github.com>
2021-08-17 17:42:20 +0100

    bump libs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,18 +7,18 @@ object Versions {
   val shadow = "7.0.0"
   val testLogger = "3.0.0"
 
-  val aws = "2.16.80"
+  val aws = "2.17.20"
   val awsLambdaCore = "1.2.1"
   val awsLambdaEvents = "3.9.0"
   val arrow = "0.13.2"
   val clikt = "3.2.0"
-  val hoplite = "1.4.3"
-  val kotlinxCoroutines = "1.5.0"
-  val logback = "1.2.3"
-  val reactor = "3.4.6"
-  val reactorKotlin = "1.1.3"
-  val slf4j = "1.7.30"
+  val hoplite = "1.4.7"
+  val kotlinxCoroutines = "1.5.1"
+  val logback = "1.2.5"
+  val reactor = "3.4.9"
+  val reactorKotlin = "1.1.4"
+  val slf4j = "1.7.32"
 
   // tests
-  val kotest = "4.6.0"
+  val kotest = "4.6.1"
 }
