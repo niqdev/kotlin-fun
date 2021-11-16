5025c784027a3aaa20fa6fe1675e8d96a3ebbd64
niqdev <niqdev@users.noreply.github.com>
2021-11-16 19:33:04 +0000

    bump libs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,21 +1,21 @@
 object Versions {
   // plugins
-  val kotlin = "1.5.31"
-  val kotlinxSerialization = "1.3.0"
+  val kotlin = "1.6.0"
+  val kotlinxSerialization = "1.3.1"
   val kotlinter = "3.6.0"
   val shadow = "7.1.0"
-  val testLogger = "3.0.0"
+  val testLogger = "3.1.0"
 
-  val aws = "2.17.71"
+  val aws = "2.17.82"
   val awsLambdaCore = "1.2.1"
   val awsLambdaEvents = "3.10.0"
   val arrow = "1.0.0"
   val clikt = "3.3.0"
-  val hoplite = "1.4.9"
-  val kondor = "1.7.0"
+  val hoplite = "1.4.14"
+  val kondor = "1.7.1"
   val kotlinxCoroutines = "1.5.2"
   val logback = "1.3.0-alpha10"
-  val reactor = "3.4.11"
+  val reactor = "3.4.12"
   val reactorKotlin = "1.1.5"
   val slf4j = "2.0.0-alpha5"
 
