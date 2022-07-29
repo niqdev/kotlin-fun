67a62fb63ed28e7fd160a508b26454c96dcc3433
niqdev <niqdev@users.noreply.github.com>
2022-07-29 17:55:03 +0100

    update libs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,29 +1,29 @@
 object Versions {
   // plugins
   val kotlin = "1.7.10"
-  val kotlinxSerialization = "1.3.3"
   val kotlinter = "3.10.0" // FIXME 3.11.1
   val shadow = "7.1.2"
   val testLogger = "3.2.0"
 
-  val aws = "2.17.228"
-  val awsKotlin = "0.16.7-beta"
+  val aws = "2.17.242"
+  val awsKotlin = "0.17.2-beta"
   val awsLambdaCore = "1.2.1"
   val awsLambdaEvents = "3.11.0"
   val arrow = "1.1.2"
   val arrowMeta = "1.6.1-alpha.5"
   val clikt = "3.5.0"
   val everit = "1.14.1"
-  val hoplite = "2.3.1"
+  val hoplite = "2.4.0"
   val jdom = "2.0.6.1"
   val kondor = "1.7.7"
-  val kotlinxCoroutines = "1.6.3"
+  val kotlinxCoroutines = "1.6.4"
+  val kotlinxSerialization = "1.3.3"
   val logback = "1.3.0-alpha16"
   val pwall = "0.36"
-  val reactor = "3.4.19"
-  val reactorKotlin = "1.1.6"
+  val reactor = "3.4.21"
+  val reactorKotlin = "1.1.7"
   val slf4j = "2.0.0-alpha7"
 
   // tests
-  val kotest = "5.3.2"
+  val kotest = "5.4.1"
 }
