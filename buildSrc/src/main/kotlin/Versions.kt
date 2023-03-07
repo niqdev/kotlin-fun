dd0a0a1a96e6e4c164806037a430f00bebfd01ad
niqdev <niqdev@users.noreply.github.com>
2023-03-07 14:58:31 +0100

    update libs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,31 +1,31 @@
 object Versions {
   // plugins
-  const val kotlin = "1.8.0"
+  const val kotlin = "1.8.10"
   const val kotlinter = "3.10.0" // FIXME 3.12.0
-  const val shadow = "7.1.2"
+  const val shadow = "8.1.0"
   const val testLogger = "3.2.0"
 
-  const val aws = "2.19.27"
-  const val awsKotlin = "0.19.5-beta"
+  const val aws = "2.20.18"
+  const val awsKotlin = "0.21.1-beta"
   const val awsLambdaCore = "1.2.2"
   const val awsLambdaEvents = "3.11.0"
   const val arrow = "1.1.5"
-  const val arrowMeta = "1.6.1-alpha.5"
-  const val clikt = "3.5.1"
-  const val confluent = "7.3.1"
+  const val arrowMeta = "1.6.1"
+  const val clikt = "3.5.2"
+  const val confluent = "7.3.2"
   const val everit = "1.14.1"
-  const val hoplite = "2.7.1"
+  const val hoplite = "2.7.2"
   const val jdom = "2.0.6.1"
   const val kondor = "1.7.7" // FIXME 1.8.0
   const val kotlinxCoroutines = "1.6.4"
-  const val kotlinxSerialization = "1.5.0-RC"
+  const val kotlinxSerialization = "1.5.0"
   const val logback = "1.4.5"
   const val pwall = "0.39"
-  const val reactor = "3.5.2"
+  const val reactor = "3.5.3"
   const val reactorKotlin = "1.2.1"
   const val slf4j = "2.0.6"
 
   // tests
-  const val kotest = "5.5.4"
+  const val kotest = "5.5.5"
   const val kotestKafka = "1.0.6"
 }
