96d68a63b1fa7522a55aa14ed410d867b113469d
niqdev <niqdev@users.noreply.github.com>
2023-09-27 14:39:23 +0100

    update stable versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,34 +1,34 @@
 object Versions {
   // plugins
-  const val kotlin = "1.9.0"
+  const val kotlin = "1.9.10"
   const val kotlinter = "3.10.0" // FIXME 3.12.0
   const val shadow = "8.1.1"
   const val testLogger = "3.2.0"
 
-  const val aws = "2.20.103"
-  const val awsKotlin = "0.28.2-beta"
-  const val awsLambdaCore = "1.2.2"
-  const val awsLambdaEvents = "3.11.2"
-  const val arrow = "1.2.0"
+  const val aws = "2.20.153"
+  const val awsKotlin = "0.32.2-beta"
+  const val awsLambdaCore = "1.2.3"
+  const val awsLambdaEvents = "3.11.3"
+  const val arrow = "1.2.1"
   const val arrowMeta = "1.6.2"
-  const val clikt = "4.1.0"
+  const val clikt = "4.2.0"
   const val confluent = "7.3.3" // FIXME 7.4.0
   const val everit = "1.14.2"
-  const val hoplite = "2.7.4"
+  const val hoplite = "2.7.5"
   const val jackson = "2.15.2"
   const val jdom = "2.0.6.1"
   const val kondor = "1.7.7" // FIXME 2.0.0
-  const val kotlinxCoroutines = "1.7.2"
-  const val kotlinxSerialization = "1.5.1"
-  const val ktor = "2.3.2"
-  const val logback = "1.4.8"
-  const val pwall = "0.40"
-  const val reactor = "3.5.8"
+  const val kotlinxCoroutines = "1.7.3"
+  const val kotlinxSerialization = "1.6.0"
+  const val ktor = "2.3.4"
+  const val logback = "1.4.11"
+  const val pwall = "0.42"
+  const val reactor = "3.5.10"
   const val reactorKotlin = "1.2.2"
-  const val slf4j = "2.0.7"
+  const val slf4j = "2.0.9"
   const val zjsonpatch = "0.4.14"
 
   // tests
-  const val kotest = "5.6.2"
-  const val kotestKafka = "2.0.0"
+  const val kotest = "5.7.2"
+  const val kotestKafka = "2.0.0" // deprecated, see testcontainers
 }
