10d0580e1519639cec2803c587b6451998b20e6c
niqdev <niqdev@users.noreply.github.com>
2024-02-09 13:29:37 +0000

    update freeb spec

--- modules/bool/src/test/kotlin/com/github/niqdev/example/FreeBSpec.kt
+++ modules/bool/src/test/kotlin/com/github/niqdev/example/FreeBSpec.kt
@@ -8,9 +8,12 @@ class FreeBSpec : WordSpec({
   "FreeB" should {
 
     "example" {
-      val valid = pure(Predicate.AtLeast13(15)) and pure(Predicate.NonEmptyName("martin"))
+      val valid = FreeB.And(
+        FreeB.Pure(Predicate.AtLeast13(18)),
+        FreeB.Pure(Predicate.NonEmptyName("martin"))
+      )
       valid.run(Predicate.eval) shouldBe true
-      valid.pretty() shouldBe "(at-least-13=15 && non-empty=martin)"
+      valid.pretty() shouldBe "(at-least-13=18 && non-empty=martin)"
 
       FreeB.And(
         FreeB.Pure(Predicate.AtLeast13(5)),
@@ -18,9 +21,28 @@ class FreeBSpec : WordSpec({
       ).run(Predicate.eval) shouldBe false
 
       FreeB.And(
-        FreeB.Pure(Predicate.AtLeast13(13)),
+        FreeB.Pure(Predicate.AtLeast13(18)),
         FreeB.Pure(Predicate.NonEmptyName(""))
       ).run(Predicate.eval) shouldBe false
+    }
+
+    "eval many" {
+      data class Person(val name: String, val age: Int)
+
+      fun isAllowed(person: Person): FreeB<Predicate> =
+        pure(Predicate.AtLeast13(person.age)) and pure(Predicate.NonEmptyName(person.name))
+
+      isAllowed(Person("martin", 18)).run(Predicate.eval) shouldBe true
+
+      val people = listOf(
+        Person("martin", 18),
+        Person("alice", 12),
+        Person("bob", 15),
+      )
+
+      people.fold(true) { allAllowed, p ->
+        allAllowed && isAllowed(p).run(Predicate.eval)
+      } shouldBe false
     }
   }
 })
